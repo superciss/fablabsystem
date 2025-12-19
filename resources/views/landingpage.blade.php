8e70bb790085baa229780affcc0be0afe9502f98
Francis <fslntsho@gmail.com>
2025-12-20 07:16:48 +0800

    landing-page-fix

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -11,7 +11,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/landingpage.css') }}">
+    <link rel="stylesheet" href="public/css/landingpage.css">
 </head>
 <body>
     <!-- Navigation -->
